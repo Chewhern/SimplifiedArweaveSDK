a6b51b858bb6c3b4e23af0a598c6973dd8804ec5
Chrono <chrono_legionnaire@protonmail.com>
2025-10-27 23:26:58 +0800

    Add files via upload

---
+++ Version/0.0.1/ArweaveModel/GetTransactionOffsetModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedArweaveSDK.ArweaveModel
+{
+    public class GetTransactionOffsetModel
+    {
+        public String offset { get; set; }
+
+        public String size { get; set; }
+    }
+}
---
+++ Version/0.0.1/ArweaveModel/TransactionStatusModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedArweaveSDK.ArweaveModel
+{
+    public class TransactionStatusModel
+    {
+        public long block_height { get; set; }
+
+        public String block_indep_hash { get; set; }
+
+        public long number_of_confirmations { get; set; }
+    }
+}
