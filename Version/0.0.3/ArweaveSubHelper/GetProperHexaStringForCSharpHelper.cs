a3fd4a460618fbad5435eb6f28d62beee0db3caf
Chrono <chrono_legionnaire@protonmail.com>
2026-03-14 12:48:00 +0800

    Add files via upload

---
+++ Version/0.0.3/ArweaveSubHelper/GetProperHexaStringForCSharpHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedArweaveSDK.ArweaveSubHelper
+{
+    public static class GetProperHexaStringForCSharpHelper
+    {
+        public static String FormatHexString(string input)
+        {
+            String FormattedHexString = "";
+            int Loop = 0;
+            while (Loop < input.Length)
+            {
+                if (Loop + 4 > input.Length)
+                {
+                    FormattedHexString += "0x" + input.Substring(Loop, 2);
+                }
+                else
+                {
+                    FormattedHexString += "0x" + input.Substring(Loop, 2) + ",";
+                }
+                Loop += 2;
+            }
+            return FormattedHexString;
+        }
+    }
+}
