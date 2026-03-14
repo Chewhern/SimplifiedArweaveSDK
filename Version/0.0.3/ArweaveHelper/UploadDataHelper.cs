bdb15b3b6f0eaff3dafb6f14bba4e3f49fe3d3e9
Chrono <chrono_legionnaire@protonmail.com>
2026-03-14 12:42:55 +0800

    Add files via upload

---
+++ Version/0.0.3/ArweaveHelper/UploadDataHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedArweaveSDK.ArweaveHelper
+{
+    public static class UploadDataHelper
+    {
+        public static async Task<string> UploadData(string Data)
+        {
+            string StatusString = "";
+
+            var httpClient = new HttpClient();
+            var content = new StringContent(Data);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await httpClient.PostAsync("https://arweave.net/tx", content);
+
+            HttpStatusCode MyStatusCode = response.StatusCode;
+
+            if (MyStatusCode == HttpStatusCode.OK)
+            {
+                StatusString = "OK";
+            }
+            else if (MyStatusCode == HttpStatusCode.AlreadyReported)
+            {
+                StatusString = "Transaction already processed.";
+            }
+            else if (MyStatusCode == HttpStatusCode.BadRequest)
+            {
+                StatusString = "Transaction verification failed.";
+            }
+            else if (MyStatusCode == HttpStatusCode.TooManyRequests)
+            {
+                StatusString = "Too Many Requests";
+            }
+            else
+            {
+                StatusString = "Transaction verification failed.";
+            }
+
+            return StatusString;
+        }
+    }
+}
